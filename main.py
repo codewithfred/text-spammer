5c3d149a4c07144fd426fdeb1a50d2997aef9c60
syx309 <alfredsoegiarto@gmail.com>
2021-07-20 11:01:54 +0700

    add emoji to spammer

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-import pyautogui, time
+# inget di import dulu
+import pyautogui, time, emoji, pyperclip
+
 # atur lama mulainya sesuai waktu yang dibutuhin untuk pindah ke halaman yang akan
 # kalian spam, misal buka line butuh 20 detik ya set dlu jadi 20 biar kalian bisa
 # bukan line sebelum botnya ngespam
 time.sleep(1)
-for i in range(20):
-  pyautogui.typewrite("Enter the sentence u want me to spam")
+
+text = "Testing emoji: " + emoji.emojize(":red_heart:")
+pyperclip.copy(text)
+
+for i in range(5):
+  # emoji harus di paste dari clipboard
+  pyautogui.hotkey("ctrl", "v")
+  pyautogui.press("enter")
+
+  # text biasa bisa di typewrite kek gini
+  pyautogui.typewrite("Spam")
   pyautogui.press("enter")
